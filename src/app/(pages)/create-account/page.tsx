85b84caae5c4b00a6380193b9d806ab49f5ad958
letuan03zz <letuanbl2003@gmail.com>
2024-07-17 02:26:01 +0700

    update

--- src/app/(pages)/create-account/page.tsx
+++ src/app/(pages)/create-account/page.tsx
@@ -23,7 +23,7 @@ export default async function CreateAccount() {
       <div className={classes.heroImg}>
         <Link href="/">
           <Image
-            src="/logo-black.svg"
+            src="/logo-white.svg"
             alt="logo"
             width={250}
             height={23}
--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -21,7 +21,7 @@ export default async function Login() {
       <div className={classes.heroImg}>
         <Link href="/">
           <Image
-            src="/logo-black.svg"
+            src="/logo-white.svg"
             alt="logo"
             width={250}
             height={23}
